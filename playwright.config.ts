dd97bb2c1e275472d0442b131120208384582ba7
Kaho Shibuya <kshibuya31@gmail.com>
2026-07-10 16:14:43 -0700

    fix junit error

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
       ['html', { outputFolder: 'execution-reports/html', open: 'on-failure'}],
-      ['junit', { outputFile: 'execution-reports/junit' }]
+      ['junit', { outputFile: 'execution-reports/junit/results.xml' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
